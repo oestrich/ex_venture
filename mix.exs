11dd072f6e1a5c2671db150b128eaec551951497
Eric Oestrich <eric@oestrich.org>
2017-07-12 14:42:58 -0400

    Start setting up distillery

--- mix.exs
+++ mix.exs
@@ -21,6 +21,8 @@ defmodule ExMud.Mixfile do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [
+      {:distillery, "~> 1.4", runtime: false},
+    ]
   end
 end
