619385129be7e100650346aa1e506c5e1bc8849d
Eric Oestrich <eric@oestrich.org>
2017-07-16 22:26:15 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Game.Mixfile do
   def project do
     [
       app: :ex_mud,
-      version: "0.5.0",
+      version: "0.6.0",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       build_embedded: Mix.env == :prod,
