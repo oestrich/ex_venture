b81b4800958ba906f87bea356fff6fc8d365175c
Eric Oestrich <eric@oestrich.org>
2017-08-03 09:00:42 -0400

    Helps to include color.js

---
+++ assets/js/color.js
@@ -0,0 +1,22 @@
+function formatColor(string) {
+  string = string.replace(/{black}/g, "<span class='black'>")
+  string = string.replace(/{red}/g, "<span class='red'>")
+  string = string.replace(/{green}/g, "<span class='green'>")
+  string = string.replace(/{yellow}/g, "<span class='yello'>")
+  string = string.replace(/{blue}/g, "<span class='blue'>")
+  string = string.replace(/{magenta}/g, "<span class='magenta'>")
+  string = string.replace(/{cyan}/g, "<span class='cyan'>")
+  string = string.replace(/{white}/g, "<span class='white'>")
+  string = string.replace(/{\/\w+}/g, "</span>")
+  return string;
+}
+
+function formatLines(string) {
+  return string.replace(/\n/g, "<br />")
+}
+
+function format(string) {
+  return formatLines(formatColor(string));
+}
+
+export default format
