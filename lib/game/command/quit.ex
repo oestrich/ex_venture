4352e9d27f25161fa0a6ba95b0dbba23198c2a93
Eric Oestrich <eric@oestrich.org>
2017-08-22 08:59:11 -0400

    Let disconnect handle leaving the room

--- lib/game/command/quit.ex
+++ lib/game/command/quit.ex
@@ -17,11 +17,9 @@ defmodule Game.Command.Quit do
   """
   @spec run(args :: [], session :: Session.t, state :: map) :: :ok
   def run(command, session, state)
-  def run({}, session, %{socket: socket, user: user, save: save}) do
+  def run({}, _session, %{socket: socket}) do
     socket |> @socket.echo("Good bye.")
     socket |> @socket.disconnect
-
-    @room.leave(save.room_id, {:user, session, user})
 
     :ok
   end
