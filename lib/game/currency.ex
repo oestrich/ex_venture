f3d92d1cffaea2af3563303c7727dc4092c15bf4
Eric Oestrich <eric@oestrich.org>
2017-09-09 20:20:56 -0700

    Add docs for currency module

--- lib/game/currency.ex
+++ lib/game/currency.ex
@@ -1,4 +1,10 @@
 defmodule Game.Currency do
+  @moduledoc """
+  Helper module for currency
+
+  Gives `@currency` and `currency/0`
+  """
+
   @currency Application.get_env(:ex_venture, :game)[:currency]
 
   defmacro __using__(_opts) do
