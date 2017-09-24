769b594a826ea12b64c1c105dc6dba71e311f641
Eric Oestrich <eric@oestrich.org>
2017-09-24 17:44:38 -0400

    Javascript map colors

--- assets/app/js/color.js
+++ assets/app/js/color.js
@@ -6,8 +6,12 @@ function formatColor(string) {
   string = string.replace(/{blue}/g, "<span class='blue'>")
   string = string.replace(/{magenta}/g, "<span class='magenta'>")
   string = string.replace(/{cyan}/g, "<span class='cyan'>")
-  string = string.replace(/{white}/g, "<span class='white'>")
-  string = string.replace(/{\/\w+}/g, "</span>")
+  string = string.replace(/{map:blue}/g, "<span class='map-blue'>")
+  string = string.replace(/{map:brown}/g, "<span class='map-brown'>")
+  string = string.replace(/{map:dark-green}/g, "<span class='map-dark-green'>")
+  string = string.replace(/{map:green}/g, "<span class='map-green'>")
+  string = string.replace(/{map:grey}/g, "<span class='map-grey'>")
+  string = string.replace(/{\/[\w:-]+}/g, "</span>")
   return string;
 }
 
