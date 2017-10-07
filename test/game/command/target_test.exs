a711c0d29cccdefddbc81120dcb6475a28d4424e
Eric Oestrich <eric@oestrich.org>
2017-10-07 16:00:47 -0400

    Clean up whitespace in the target command test

--- test/game/command/target_test.exs
+++ test/game/command/target_test.exs
@@ -23,7 +23,7 @@ defmodule Game.Command.TargetTest do
 
     assert state.target == {:npc, 1}
     [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(now targeting), look) 
+    assert Regex.match?(~r(now targeting), look)
   end
 
   test "targeting another player", %{session: session, socket: socket, user: user} do
@@ -31,7 +31,7 @@ defmodule Game.Command.TargetTest do
 
     assert state.target == {:user, 2}
     [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(now targeting), look) 
+    assert Regex.match?(~r(now targeting), look)
   end
 
   test "cannot target another player if health is < 1", %{session: session, socket: socket, user: user} do
@@ -43,28 +43,28 @@ defmodule Game.Command.TargetTest do
     :ok = Game.Command.Target.run({"player"}, session, %{socket: socket, user: user, save: %{room_id: 1}})
 
     [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(could not be targeted), look) 
+    assert Regex.match?(~r(could not be targeted), look)
   end
 
   test "target not found", %{session: session, socket: socket, user: user} do
     :ok = Game.Command.Target.run({"unknown"}, session, %{socket: socket, user: user, save: %{room_id: 1}})
 
     [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(not find), look) 
+    assert Regex.match?(~r(not find), look)
   end
 
   test "viewing your target - npc", %{session: session, socket: socket} do
     :ok = Game.Command.Target.run({}, session, %{socket: socket, save: %{room_id: 1}, target: {:npc, 1}})
 
     [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(Your target is), look) 
+    assert Regex.match?(~r(Your target is), look)
   end
 
   test "viewing your target - npc no longer there", %{session: session, socket: socket} do
     :ok = Game.Command.Target.run({}, session, %{socket: socket, save: %{room_id: 1}, target: {:npc, 2}})
 
     [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(Your target could not be found), look) 
+    assert Regex.match?(~r(Your target could not be found), look)
   end
 
   test "viewing your target - user", %{session: session, socket: socket} do
@@ -78,13 +78,13 @@ defmodule Game.Command.TargetTest do
     :ok = Game.Command.Target.run({}, session, %{socket: socket, save: %{room_id: 1}, target: {:user, 3}})
 
     [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(Your target could not be found), look) 
+    assert Regex.match?(~r(Your target could not be found), look)
   end
 
   test "viewing your target - missing", %{session: session, socket: socket} do
     :ok = Game.Command.Target.run({}, session, %{socket: socket, save: %{room_id: 1}, target: nil})
 
     [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(You don't have a target), look) 
+    assert Regex.match?(~r(You don't have a target), look)
   end
 end
