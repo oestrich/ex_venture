be9b937d1f90d7db93f9f51498a9810df2968b69
Eric Oestrich <eric@oestrich.org>
2017-11-06 08:05:37 -0500

    Display wisdom on the public races page

--- lib/web/views/race_view.ex
+++ lib/web/views/race_view.ex
@@ -1,3 +1,11 @@
 defmodule Web.RaceView do
   use Web, :view
+
+  alias Data.Stats
+
+  def stat(%{starting_stats: stats}, field) do
+    stats
+    |> Stats.default()
+    |> Map.get(field)
+  end
 end
