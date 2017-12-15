358ba6fc006c243cea191a8ac715a5161362e198
Eric Oestrich <eric@oestrich.org>
2017-12-15 10:40:47 -0500

    Add docs to Game.Channels and refactor the GenServer to be split out

--- lib/game/channel.ex
+++ lib/game/channel.ex
@@ -1,11 +1,24 @@
 defmodule Game.Channel do
   @moduledoc """
-  Communication channels
+  Communication channels.
+
+  The joining process will need to have the following `handle_info` cases set up:
+  - `{:channel, {:joined, channel}}`
+  - `{:channel, {:left, channel}}`
+  - `{:channel, {:broadcast, message}}`
+  - `{:channel, {:tell, from, message}}`
   """
 
   use GenServer
 
+  alias Data.User
+  alias Game.Message
+
   require Logger
+
+  defstruct [:channels, :tells]
+
+  @type t :: %__MODULE__{}
 
   def start_link() do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
@@ -25,6 +38,10 @@ defmodule Game.Channel do
     GenServer.cast(__MODULE__, {:join, channel, self()})
   end
 
+  @doc """
+  Join the user's private tell channel
+  """
+  @spec join_tell(user :: User.t) :: :ok
   def join_tell(user) do
     GenServer.cast(__MODULE__, {:join_tell, self(), user})
   end
@@ -71,24 +88,17 @@ defmodule Game.Channel do
 
   def init(_) do
     Process.flag(:trap_exit, true)
-    {:ok, %{channels: %{}, tells: %{}}}
+    {:ok, %__MODULE__{channels: %{}, tells: %{}}}
   end
 
-  def handle_call(:subscribed, {pid, _}, state = %{channels: channels}) do
-    channels = channels
-    |> Enum.filter(fn ({_channel, pids}) -> Enum.member?(pids, pid) end)
-    |> Enum.map(fn ({channel, _pids}) -> channel end)
+  def handle_call(:subscribed, {pid, _}, state) do
+    channels = subscribed_channels(state, pid)
     {:reply, channels, state}
   end
 
-  def handle_cast({:join, channel, pid}, state = %{channels: channels}) do
-    Process.link(pid)
-
-    channel_pids = Map.get(channels, channel, [])
-    channels = Map.put(channels, channel, [pid | channel_pids])
-
-    send(pid, {:channel, {:joined, channel}})
-    {:noreply, Map.put(state, :channels, channels)}
+  def handle_cast({:join, channel, pid}, state) do
+    state = join_channel(state, channel, pid)
+    {:noreply, state}
   end
 
   def handle_cast({:join_tell, pid, user}, state = %{tells: tells}) do
@@ -96,18 +106,76 @@ defmodule Game.Channel do
     {:noreply, Map.put(state, :tells, tells)}
   end
 
-  def handle_cast({:leave, channel, pid}, state = %{channels: channels}) do
+  def handle_cast({:leave, channel, pid}, state) do
+    state = leave_channel(state, channel, pid)
+    {:noreply, state}
+  end
+
+  def handle_cast({:broadcast, channel, message}, state) do
+    broadcast(state, channel, message)
+    {:noreply, state}
+  end
+
+  def handle_cast({:tell, user, from, message}, state) do
+    tell(state, user, from, message)
+    {:noreply, state}
+  end
+
+  def handle_info({:EXIT, pid, _reason}, state) do
+    state = process_died(state, pid)
+    {:noreply, state}
+  end
+
+  #
+  # Server Implementation
+  #
+
+  @doc """
+  Get a list of channels the pid is subscribed to
+  """
+  @spec subscribed_channels(map, pid) :: [String.t]
+  def subscribed_channels(%{channels: channels}, pid) do
+    channels
+    |> Enum.filter(fn ({_channel, pids}) -> Enum.member?(pids, pid) end)
+    |> Enum.map(fn ({channel, _pids}) -> channel end)
+  end
+
+  @doc """
+  Join a channel
+
+  Adds the pid to the channel's list of pids for broadcasting to. Will also send
+  back to the process that the join was successful.
+  """
+  @spec join_channel(t(), String.t, pid) :: t()
+  def join_channel(state = %{channels: channels}, channel, pid) do
+    Process.link(pid)
+    channel_pids = Map.get(channels, channel, [])
+    channels = Map.put(channels, channel, [pid | channel_pids])
+    send(pid, {:channel, {:joined, channel}})
+    Map.put(state, :channels, channels)
+  end
+
+  @doc """
+  Leave a channel
+
+  Removes the pid from the channel's list of pids. Will also send back to the process
+  after the leave was successful.
+  """
+  @spec leave_channel(t(), String.t(), pid()) :: t()
+  def leave_channel(state = %{channels: channels}, channel, pid) do
     channel_pids = channels
     |> Map.get(channel, [])
     |> Enum.reject(&(&1 == pid))
-
-    send(pid, {:channel, {:left, channel}})
-
     channels = Map.put(channels, channel, channel_pids)
-    {:noreply, Map.put(state, :channels, channels)}
+    send(pid, {:channel, {:left, channel}})
+    Map.put(state, :channels, channels)
   end
 
-  def handle_cast({:broadcast, channel, message}, state = %{channels: channels}) do
+  @doc """
+  Broadcast a message to a channel
+  """
+  @spec broadcast(t(), String.t(), Message.t()) :: :ok
+  def broadcast(%{channels: channels}, channel, message) do
     Logger.info("Channel '#{channel}' message: #{inspect(message)}", type: :channel)
 
     channels
@@ -115,20 +183,28 @@ defmodule Game.Channel do
     |> Enum.each(fn (pid) ->
       send(pid, {:channel, {:broadcast, message}})
     end)
-
-    {:noreply, state}
   end
 
-  def handle_cast({:tell, user, from, message}, state = %{tells: tells}) do
+  @doc """
+  Send a tell to a user
+
+  A message will be sent to the user's session in the form of `{:channel, {:tell, from, message}}`.
+  """
+  @spec tell(t(), User.t(), User.t(), Message.t()) :: :ok
+  def tell(%{tells: tells}, user, from, message) do
     case tells |> Map.get("tells:#{user.id}", nil) do
       nil -> nil
       pid -> send(pid, {:channel, {:tell, from, message}})
     end
-
-    {:noreply, state}
   end
 
-  def handle_info({:EXIT, pid, _reason}, state = %{channels: channels, tells: tells}) do
+  @doc """
+  The session process died, due to a crash or the player quitting.
+
+  Leave all channels and their user tell channel.
+  """
+  @spec process_died(t(), pid()) :: t()
+  def process_died(state = %{channels: channels, tells: tells}, pid) do
     channels = Enum.reduce(channels, %{}, fn ({channel, pids}, channels) ->
       pids = pids |> Enum.reject(&(&1 == pid))
       Map.put(channels, channel, pids)
@@ -138,10 +214,8 @@ defmodule Game.Channel do
     |> Enum.reject(fn ({_, tell_pid}) -> tell_pid == pid end)
     |> Enum.into(%{})
 
-    state = state
+    state
     |> Map.put(:channels, channels)
     |> Map.put(:tells, tells)
-
-    {:noreply, state}
   end
 end
