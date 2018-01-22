725a79c06c69872fbf796480b7620a7ea5d79493
Eric Oestrich <eric@oestrich.org>
2018-01-21 23:17:15 -0500

    Format Game.Room

--- lib/game/room.ex
+++ lib/game/room.ex
@@ -36,15 +36,15 @@ defmodule Game.Room do
   @doc """
   Load all rooms in the database
   """
-  @spec all() :: [map]
+  @spec all() :: [map()]
   def all() do
-    Repo.all
+    Repo.all()
   end
 
   @doc """
   Load all rooms for a zone
   """
-  @spec for_zone(zone_id :: integer) :: [map]
+  @spec for_zone(integer()) :: [map()]
   def for_zone(zone_id) do
     Repo.for_zone(zone_id)
   end
@@ -54,6 +54,7 @@ defmodule Game.Room do
 
   Fetches current room
   """
+  @spec look(integer()) :: Room.t()
   def look(id) do
     GenServer.call(pid(id), :look)
   end
@@ -65,6 +66,7 @@ defmodule Game.Room do
   """
   @spec enter(integer(), Character.t(), atom()) :: :ok
   def enter(id, character, reason \\ :enter)
+
   def enter(id, character, reason) do
     GenServer.cast(pid(id), {:enter, character, reason})
   end
@@ -76,6 +78,7 @@ defmodule Game.Room do
   """
   @spec leave(integer(), Character.t(), atom()) :: :ok
   def leave(id, character, reason \\ :leave)
+
   def leave(id, character, reason) do
     GenServer.cast(pid(id), {:leave, character, reason})
   end
@@ -83,7 +86,7 @@ defmodule Game.Room do
   @doc """
   Say to the players in the room
   """
-  @spec say(id :: integer, sender :: pid, message :: Message.t) :: :ok
+  @spec say(integer(), pid(), Message.t()) :: :ok
   def say(id, sender, message) do
     GenServer.cast(pid(id), {:say, sender, message})
   end
@@ -99,7 +102,7 @@ defmodule Game.Room do
   @doc """
   Update the character after a stats change
   """
-  @spec update_character(id :: integer, character :: tuple) :: :ok
+  @spec update_character(integer(), tuple()) :: :ok
   def update_character(id, character) do
     GenServer.cast(pid(id), {:update_character, character})
   end
@@ -107,7 +110,7 @@ defmodule Game.Room do
   @doc """
   Pick up the item
   """
-  @spec pick_up(id :: integer, item :: Item.t) :: :ok
+  @spec pick_up(integer(), Item.t()) :: :ok
   def pick_up(id, item) do
     GenServer.call(pid(id), {:pick_up, item})
   end
@@ -115,7 +118,7 @@ defmodule Game.Room do
   @doc """
   Pick up currency
   """
-  @spec pick_up_currency(id :: integer) :: :ok
+  @spec pick_up_currency(integer()) :: :ok
   def pick_up_currency(id) do
     GenServer.call(pid(id), :pick_up_currency)
   end
@@ -123,7 +126,7 @@ defmodule Game.Room do
   @doc """
   Drop an item into a room
   """
-  @spec drop(id :: integer, who :: {atom, map}, item :: Item.t) :: :ok
+  @spec drop(integer(), Character.t(), Item.t()) :: :ok
   def drop(id, who, item) do
     GenServer.cast(pid(id), {:drop, who, item})
   end
@@ -131,7 +134,7 @@ defmodule Game.Room do
   @doc """
   Drop currency into a room
   """
-  @spec drop_currency(id :: integer, who :: {atom, map}, current :: integer) :: :ok
+  @spec drop_currency(integer(), Character.t(), integer()) :: :ok
   def drop_currency(id, who, currency) do
     GenServer.cast(pid(id), {:drop_currency, who, currency})
   end
@@ -139,7 +142,7 @@ defmodule Game.Room do
   @doc """
   Send a tick to the room
   """
-  @spec tick(id :: integer, time :: DateTime.t) :: :ok
+  @spec tick(integer(), DateTime.t()) :: :ok
   def tick(id, _time) do
     GenServer.cast(pid(id), :tick)
   end
@@ -147,7 +150,7 @@ defmodule Game.Room do
   @doc """
   Update a room's data
   """
-  @spec update(id :: integer, room :: Room.t) :: :ok
+  @spec update(integer(), Room.t()) :: :ok
   def update(id, room) do
     GenServer.cast(pid(id), {:update, room})
   end
@@ -196,7 +199,9 @@ defmodule Game.Room do
     end
   end
 
-  def handle_cast({:enter, {:user, user}, reason}, state = %{room: room, players: players, npcs: npcs}) do
+  def handle_cast({:enter, {:user, user}, reason}, state) do
+    %{room: room, players: players, npcs: npcs} = state
+
     Logger.info("Player (#{user.id}) entered room (#{room.id})", type: :room)
 
     players |> inform_players({"room/entered", {{:user, user}, reason}})
@@ -205,7 +210,9 @@ defmodule Game.Room do
     {:noreply, Map.put(state, :players, [user | players])}
   end
 
-  def handle_cast({:enter, {:npc, npc}, reason}, state = %{room: room, npcs: npcs, players: players}) do
+  def handle_cast({:enter, {:npc, npc}, reason}, state) do
+    %{room: room, players: players, npcs: npcs} = state
+
     Logger.info("NPC (#{npc.id}) entered room (#{room.id})", type: :room)
 
     players |> inform_players({"room/entered", {{:npc, npc}, reason}})
@@ -214,7 +221,9 @@ defmodule Game.Room do
     {:noreply, Map.put(state, :npcs, [npc | npcs])}
   end
 
-  def handle_cast({:leave, {:user, user}, reason}, state = %{room: room, players: players, npcs: npcs}) do
+  def handle_cast({:leave, {:user, user}, reason}, state) do
+    %{room: room, players: players, npcs: npcs} = state
+
     Logger.info("Player (#{user.id}) left room (#{room.id})", type: :room)
 
     players = Enum.reject(players, &(&1.id == user.id))
@@ -224,7 +233,9 @@ defmodule Game.Room do
     {:noreply, Map.put(state, :players, players)}
   end
 
-  def handle_cast({:leave, {:npc, npc}, reason}, state = %{room: room, players: players, npcs: npcs}) do
+  def handle_cast({:leave, {:npc, npc}, reason}, state) do
+    %{room: room, players: players, npcs: npcs} = state
+
     Logger.info("NPC (#{npc.id}) left room (#{room.id})", type: :room)
 
     npcs = Enum.reject(npcs, &(&1.id == npc.id))
@@ -235,8 +246,9 @@ defmodule Game.Room do
   end
 
   def handle_cast({:say, {:user, sender}, message}, state = %{players: players, npcs: npcs}) do
+    # don't send to the sender
     players
-    |> Enum.reject(&(&1.id == sender.id)) # don't send to the sender
+    |> Enum.reject(&(&1.id == sender.id))
     |> echo_to_players(message.formatted)
 
     npcs |> inform_npcs({"room/heard", message})
@@ -247,47 +259,54 @@ defmodule Game.Room do
   def handle_cast({:say, {:npc, sender}, message}, state = %{players: players, npcs: npcs}) do
     players |> echo_to_players(message.formatted)
 
+    # don't send to the sender
     npcs
-    |> Enum.reject(&(&1.id == sender.id)) # don't send to the sender
+    |> Enum.reject(&(&1.id == sender.id))
     |> inform_npcs({"room/heard", message})
 
     {:noreply, state}
   end
 
   def handle_cast({:emote, {:user, sender}, message}, state = %{players: players, npcs: npcs}) do
+    # don't send to the sender
     players
-    |> Enum.reject(&(&1.id == sender.id)) # don't send to the sender
+    |> Enum.reject(&(&1.id == sender.id))
     |> echo_to_players(message.formatted)
 
     npcs |> inform_npcs({"room/heard", message})
 
     {:noreply, state}
   end
+
   def handle_cast({:emote, {:npc, sender}, message}, state = %{players: players, npcs: npcs}) do
     players |> echo_to_players(message.formatted)
 
+    # don't send to the sender
     npcs
-    |> Enum.reject(&(&1.id == sender.id)) # don't send to the sender
+    |> Enum.reject(&(&1.id == sender.id))
     |> inform_npcs({"room/heard", message})
 
     {:noreply, state}
   end
 
   def handle_cast({:update_character, {:user, user}}, state = %{players: players}) do
-    case Enum.member?(Enum.map(players, &(&1.id)), user.id) do
+    case Enum.member?(Enum.map(players, & &1.id), user.id) do
       true ->
         players = players |> Enum.reject(&(&1.id == user.id))
         players = [user | players]
         {:noreply, Map.put(state, :players, players)}
+
       false ->
         {:noreply, state}
     end
   end
+
   def handle_cast({:update_character, {:npc, npc}}, state = %{npcs: npcs}) do
     case Enum.find(npcs, &(&1.id == npc.id)) do
       nil ->
         GenServer.cast(self(), {:enter, {:npc, npc}})
         {:noreply, state}
+
       _npc ->
         npcs = npcs |> Enum.reject(&(&1.id == npc.id))
         npcs = [npc | npcs]
@@ -299,47 +318,61 @@ defmodule Game.Room do
     case Actions.drop(room, instance) do
       {:ok, room} ->
         item = Items.item(instance)
-        Logger.info("Character (#{elem(who, 0)}, #{elem(who, 1).id}) dropped item (#{item.id})", type: :room)
+
+        Logger.info(
+          "Character (#{elem(who, 0)}, #{elem(who, 1).id}) dropped item (#{item.id})",
+          type: :room
+        )
+
         players |> echo_to_players(Format.dropped(who, item))
         {:noreply, Map.put(state, :room, room)}
-      _ -> {:noreply, state}
+
+      _ ->
+        {:noreply, state}
     end
   end
 
   def handle_cast({:drop_currency, who, amount}, state = %{room: room, players: players}) do
     case Actions.drop_currency(room, amount) do
       {:ok, room} ->
-        Logger.info("Character (#{elem(who, 0)}, #{elem(who, 1).id}) dropped #{amount} currency", type: :room)
+        Logger.info(
+          "Character (#{elem(who, 0)}, #{elem(who, 1).id}) dropped #{amount} currency",
+          type: :room
+        )
+
         players |> echo_to_players(who, Format.dropped(who, {:currency, amount}))
         {:noreply, Map.put(state, :room, room)}
-      _ -> {:noreply, state}
+
+      _ ->
+        {:noreply, state}
     end
   end
 
   defp echo_to_players(players, {:user, user}, message) do
     players
-    |> Enum.reject(fn ({_, _, player}) -> player.id == user.id end)
+    |> Enum.reject(fn {_, _, player} -> player.id == user.id end)
     |> echo_to_players(message)
   end
+
   defp echo_to_players(players, _, message) do
     echo_to_players(players, message)
   end
 
   defp echo_to_players(players, message) do
-    Enum.each(players, fn (user) ->
+    Enum.each(players, fn user ->
       Session.echo(user, message)
     end)
   end
 
   defp inform_players(players, action) do
-    Enum.each(players, fn (user) ->
+    Enum.each(players, fn user ->
       Session.notify(user, action)
     end)
   end
 
   @spec inform_npcs(npcs :: list, action :: tuple) :: :ok
   defp inform_npcs(npcs, action) do
-    Enum.each(npcs, fn (npc) ->
+    Enum.each(npcs, fn npc ->
       NPC.notify(npc.id, action)
     end)
   end
