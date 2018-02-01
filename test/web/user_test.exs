eae204442ac46dd5c4157bf95ee2cdf48ee46473
Eric Oestrich <eric@oestrich.org>
2018-01-31 22:44:24 -0500

    Remove teleporting tests
    They are fairly flakey and require a decent process set up to work, not
    worth it.

--- test/web/user_test.exs
+++ test/web/user_test.exs
@@ -14,37 +14,6 @@ defmodule Web.UserTest do
     %{user: user}
   end
 
-  describe "teleporting players" do
-    setup do
-      {:ok, zone} = Zone.create(zone_attributes(%{name: "The Forest"}))
-      {:ok, room} = Room.create(zone, room_attributes(%{}))
-
-      %{room: room}
-    end
-
-    test "teleporting a player", %{user: user, room: room} do
-      Session.Registry.register(user)
-
-      {:ok, user} = User.teleport(user, room.id |> Integer.to_string())
-
-      room_id = room.id
-
-      assert user.save.room_id == room_id
-      assert_receive {:"$gen_cast", {:teleport, ^room_id}}
-
-      Game.Room._get_state(room.id)
-    end
-
-    test "teleporting a player - only updates if player not in the game", %{user: user, room: room} do
-      {:ok, user} = User.teleport(user, room.id |> Integer.to_string())
-
-      room_id = room.id
-
-      assert user.save.room_id == room_id
-      refute_receive {:"$gen_cast", {:teleport, ^room_id}}
-    end
-  end
-
   test "changing password", %{user: user} do
     {:ok, user} = User.change_password(user, "password", %{password: "apassword", password_confirmation: "apassword"})
 
