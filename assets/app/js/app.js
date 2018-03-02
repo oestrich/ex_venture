662afe2a13646bc9c3d9a17a8257efe229ec9a2a
Eric Oestrich <eric@oestrich.org>
2018-03-02 10:13:53 -0500

    Remove focus on any input to the window
    Update fsevents

--- assets/app/js/app.js
+++ assets/app/js/app.js
@@ -1,10 +1,4 @@
 import "phoenix_html"
 import {channel} from "./socket"
 
-let inputFocus = function() {
-  document.getElementById("prompt").focus();
-}
-
-window.onkeydown = inputFocus;
-
 channel.join()
