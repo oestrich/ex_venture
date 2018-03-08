aa9f5992959b0be694a68b8691191faf4ff860c6
Eric Oestrich <eric@oestrich.org>
2018-03-08 17:37:30 -0500

    Fix broken tests from npc control panel changes

--- test/game/npc_test.exs
+++ test/game/npc_test.exs
@@ -17,13 +17,13 @@ defmodule Game.NPCTest do
     effect = %{kind: "damage", type: :slashing, amount: 10}
 
     state = %State{npc: %{id: 1, name: "NPC", stats: %{health: 25}}}
-    {:noreply, state} = NPC.handle_cast({:apply_effects, [effect], {:user, %{id: 2}}, "description"}, state)
+    {:noreply, state} = NPC.handle_cast({:apply_effects, [effect], {:user, %{id: 2, name: "Player"}}, "description"}, state)
     assert state.npc.stats.health == 15
   end
 
   test "applying continuous effects - damage over time" do
     effect = %{kind: "damage/over-time", type: :slashing, every: 10, count: 3, amount: 10}
-    from = {:user, %{id: 2}}
+    from = {:user, %{id: 2, name: "Player"}}
 
     state = %State{npc: %{id: 1, name: "NPC", stats: %{health: 25}}, continuous_effects: []}
     {:noreply, state} = NPC.handle_cast({:apply_effects, [effect], from, "description"}, state)
@@ -41,7 +41,7 @@ defmodule Game.NPCTest do
     npc = %{currency: 0, npc_items: [], id: 1, name: "NPC", stats: %{health: 10}}
     npc_spawner = %{spawn_interval: 0}
     state = %State{room_id: 1, npc: npc, npc_spawner: npc_spawner}
-    {:noreply, state} = NPC.handle_cast({:apply_effects, [effect], {:user, %{id: 2}}, "description"}, state)
+    {:noreply, state} = NPC.handle_cast({:apply_effects, [effect], {:user, %{id: 2, name: "Player"}}, "description"}, state)
     assert state.npc.stats.health == 0
 
     assert [{1, {:npc, _}, :death}] = @room.get_leaves()
