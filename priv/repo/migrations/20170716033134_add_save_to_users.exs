01fabdf66c7cbcabea66c3907af4f18cab74d5bd
Eric Oestrich <eric@oestrich.org>
2018-04-08 19:37:34 -0400

    Use to_jsonb for the starting save migration

--- priv/repo/migrations/20170716033134_add_save_to_users.exs
+++ priv/repo/migrations/20170716033134_add_save_to_users.exs
@@ -6,7 +6,7 @@ defmodule Data.Repo.Migrations.AddSaveToUsers do
       add :save, :map, default: fragment("'{}'"), null: false
     end
 
-    execute "update users set save = (select row_to_json(t) from (select id as room_id from rooms limit 1) t);"
+    execute "update users set save = (select to_jsonb(t) from (select id as room_id from rooms limit 1) t);"
   end
 
   def down do
