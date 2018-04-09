243056d7c72836833685f3c58a2f1cf1d19529ca
Eric Oestrich <eric@oestrich.org>
2018-04-08 21:16:46 -0400

    Listen in a direction

--- lib/game/command/listen.ex
+++ lib/game/command/listen.ex
@@ -5,6 +5,8 @@ defmodule Game.Command.Listen do
 
   use Game.Command
 
+  alias Data.Exit
+
   commands(["listen"], parse: false)
 
   @impl Game.Command
@@ -27,12 +29,27 @@ defmodule Game.Command.Listen do
       iex> Game.Command.Listen.parse("listen")
       {}
 
+      iex> Game.Command.Listen.parse("listen north")
+      {:north}
+
+      iex> Game.Command.Listen.parse("listen outside")
+      {:error, :bad_parse, "listen outside"}
+
       iex> Game.Command.Listen.parse("unknown")
       {:error, :bad_parse, "unknown"}
   """
   @spec parse(String.t()) :: {any()}
   def parse(command)
   def parse("listen"), do: {}
+  def parse("listen " <> direction) do
+    case Exit.exit?(direction) do
+      true ->
+        {String.to_atom(direction)}
+
+      false ->
+        {:error, :bad_parse, "listen " <> direction}
+    end
+  end
 
   @impl Game.Command
   def run(command, state)
@@ -49,7 +66,34 @@ defmodule Game.Command.Listen do
     end
   end
 
+  def run({direction}, state = %{save: save}) do
+    room = @room.look(save.room_id)
+
+    with {:ok, room} <- room |> get_exit(direction),
+         true <- room_has_noises?(room) do
+      state.socket |> @socket.echo(Format.listen_room(room))
+    else
+      {:error, :not_found} ->
+        state.socket |> @socket.echo("There is no exit that direction to listen to.")
+
+      _ ->
+        state.socket |> @socket.echo("Nothing can be heard")
+    end
+  end
+
   defp room_has_noises?(room) do
     !is_nil(room.listen) || Enum.any?(room.features, &(!is_nil(&1.listen)))
+  end
+
+  def get_exit(room, direction) do
+    id_key = String.to_atom("#{direction}_id")
+    case room |> Exit.exit_to(direction) do
+      %{^id_key => room_id} ->
+        room = @room.look(room_id)
+        {:ok, room}
+
+      _ ->
+        {:error, :not_found}
+    end
   end
 end
--- test/game/command/listen_test.exs
+++ test/game/command/listen_test.exs
@@ -1,5 +1,6 @@
 defmodule Game.Command.ListenTest do
   use Data.ModelCase
+  doctest Game.Command.Listen
 
   alias Game.Command.Listen
 
@@ -46,6 +47,38 @@ defmodule Game.Command.ListenTest do
 
       [{_socket, echo}] = @socket.get_echos()
       assert Regex.match?(~r/flapping/, echo)
+    end
+  end
+
+  describe "listening in a direction" do
+    setup do
+      room = %{id: 1, exits: [%{north_id: 2, south_id: 1}]}
+      @room.set_room(Map.merge(@room._room(), room), multiple: true)
+
+      room = %{
+        id: 2,
+        listen: "Sounds of water trickling can be heard",
+        features: [
+          %{key: "flag", listen: "A flag is flapping in the breeze"},
+        ],
+      }
+      @room.set_room(Map.merge(@room._room(), room), multiple: true)
+
+      :ok
+    end
+
+    test "includes the room listen text", %{state: state} do
+      :ok = Listen.run({:north}, state)
+
+      [{_socket, echo}] = @socket.get_echos()
+      assert Regex.match?(~r/trickling/, echo)
+    end
+
+    test "nothing in specified direction", %{state: state} do
+      :ok = Listen.run({:south}, state)
+
+      [{_socket, echo}] = @socket.get_echos()
+      assert Regex.match?(~r/no exit/i, echo)
     end
   end
 end
