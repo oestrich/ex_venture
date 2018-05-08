fe99b56c9386d67fc2e921b519a55ee8050921cc
Eric Oestrich <eric@oestrich.org>
2018-05-08 08:58:25 -0400

    Add a test for updating your email

--- test/web/controller/account_controller_test.exs
+++ test/web/controller/account_controller_test.exs
@@ -2,12 +2,36 @@ defmodule Web.AccountControllerTest do
   use Web.AuthConnCase
 
   test "change your password", %{conn: conn} do
-    conn = put conn, public_account_path(conn, :update), user: %{current_password: "password", password: "p@ssw0rd", password_confirmation: "p@ssw0rd"}
+    params = %{
+      current_password: "password",
+      password: "p@ssw0rd",
+      password_confirmation: "p@ssw0rd"
+    }
+
+    conn = put conn, public_account_path(conn, :update), user: params
+
     assert redirected_to(conn) == public_account_path(conn, :show)
   end
 
   test "bad current password", %{conn: conn} do
-    conn = put conn, public_account_path(conn, :update), user: %{current_password: "lassword", password: "p@ssw0rd", password_confirmation: "p@ssw0rd"}
+    params = %{
+      current_password: "lassword",
+      password: "p@ssw0rd",
+      password_confirmation: "p@ssw0rd"
+    }
+
+    conn = put conn, public_account_path(conn, :update), user: params
+
+    assert redirected_to(conn) == public_account_path(conn, :show)
+  end
+
+  test "update email", %{conn: conn} do
+    params = %{
+      email: "new-email@example.com",
+    }
+
+    conn = put conn, public_account_path(conn, :update), user: params
+
     assert redirected_to(conn) == public_account_path(conn, :show)
   end
 end
