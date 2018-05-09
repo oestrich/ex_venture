6f2ce10326b3bab2f50f9076641f3e8d5b314f9b
Eric Oestrich <eric@oestrich.org>
2018-05-09 00:07:11 -0400

    Pick up all items and currency in a room

--- lib/game/command/pick_up.ex
+++ lib/game/command/pick_up.ex
@@ -40,12 +40,18 @@ defmodule Game.Command.PickUp do
 
       iex> Game.Command.PickUp.parse("pick up item")
       {"item"}
+      iex> Game.Command.PickUp.parse("pick up all")
+      {:all}
 
       iex> Game.Command.PickUp.parse("get item")
       {"item"}
+      iex> Game.Command.PickUp.parse("get all")
+      {:all}
 
       iex> Game.Command.PickUp.parse("take item")
       {"item"}
+      iex> Game.Command.PickUp.parse("take all")
+      {:all}
 
       iex> Game.Command.PickUp.parse("unknown")
       {:error, :bad_parse, "unknown"}
@@ -54,6 +60,9 @@ defmodule Game.Command.PickUp do
   def parse("pick up"), do: {"pick up", :help}
   def parse("get"), do: {"get", :help}
   def parse("take"), do: {"take", :help}
+  def parse("pick up all"), do: {:all}
+  def parse("get all"), do: {:all}
+  def parse("take all"), do: {:all}
   def parse("pick up " <> item), do: {item}
   def parse("get " <> item), do: {item}
   def parse("take " <> item), do: {item}
@@ -64,27 +73,72 @@ defmodule Game.Command.PickUp do
   """
   def run(command, state)
 
-  def run({@currency}, state = %{socket: socket, save: save}) do
-    case @room.pick_up_currency(save.room_id) do
-      {:ok, currency} ->
-        Logger.info(
-          "Session (#{inspect(self())}) picking up #{currency} currency from room (#{save.room_id})",
-          type: :player
-        )
+  def run({@currency}, state) do
+    case pick_up_currency(state) do
+      {:ok, currency, state} ->
+        state.socket |> @socket.echo("You picked up #{currency} #{@currency}")
+        {:update, state}
 
-        save = %{save | currency: save.currency + currency}
-        socket |> @socket.echo("You picked up #{currency} #{@currency}")
-        {:update, Map.put(state, :save, save)}
-
-      _ ->
-        socket |> @socket.echo(~s("#{@currency}" could not be found))
-        :ok
+      {:error, :could_not_pickup} ->
+        state.socket |> @socket.echo(~s("#{@currency}" could not be found))
     end
   end
 
-  def run({item_name}, state = %{socket: socket, save: %{room_id: room_id}}) do
-    room = @room.look(room_id)
+  def run({:all}, state) do
+    %{save: save} = state
+    room = @room.look(save.room_id)
 
+    with {:ok, state} <- pick_up_all_items(state, room),
+         {:ok, currency, state} <- pick_up_currency(state) do
+      state.socket |> @socket.echo("You picked up #{currency} #{@currency}")
+      {:update, state}
+    else
+      {:error, :could_not_pickup} ->
+        {:update, state}
+    end
+  end
+
+  def run({item_name}, state = %{save: save}) do
+    room = @room.look(save.room_id)
+
+    with {:ok, instance} <- find_item(room, item_name),
+         {:ok, item, state} <- pick_up(instance, room, state) do
+      state.socket |> @socket.echo("You picked up the #{Format.item_name(item)}")
+      {:update, state}
+    else
+      {:error, :not_found} ->
+        state.socket |> @socket.echo(~s("#{item_name}" could not be found))
+
+      {:error, :could_not_pickup, item} ->
+        state.socket |> @socket.echo(~s("#{item.name}" could not be found))
+    end
+  end
+
+  def run({verb, :help}, %{socket: socket}) do
+    message =
+      "You don't know what to #{verb}. See {command}help get{/command} for more information."
+
+    socket |> @socket.echo(message)
+  end
+
+  defp pick_up_all_items(state, room) do
+    state =
+      Enum.reduce(room.items, state, fn item, state ->
+        case pick_up(item, room, state) do
+          {:ok, item, state} ->
+            state.socket |> @socket.echo("You picked up the #{Format.item_name(item)}")
+
+            state
+
+          _ ->
+            state
+        end
+      end)
+
+    {:ok, state}
+  end
+
+  defp find_item(room, item_name) do
     instance =
       room.items
       |> Enum.find(fn instance ->
@@ -94,24 +148,17 @@ defmodule Game.Command.PickUp do
 
     case instance do
       nil ->
-        socket |> @socket.echo(~s("#{item_name}" could not be found))
-        :ok
+        {:error, :not_found}
 
       instance ->
-        pick_up(instance, room, state)
+        {:ok, instance}
     end
   end
 
-  def run({verb, :help}, %{socket: socket}) do
-    socket
-    |> @socket.echo(
-      "You don't know what to #{verb}. See {command}help get{/command} for more information."
-    )
-
-    :ok
-  end
-
-  def pick_up(item, room, state = %{socket: socket, save: save}) do
+  @doc """
+  Pick up an item from a room
+  """
+  def pick_up(item, room, state = %{save: save}) do
     case @room.pick_up(room.id, item) do
       {:ok, instance} ->
         item = Items.item(instance)
@@ -122,12 +169,28 @@ defmodule Game.Command.PickUp do
         )
 
         save = %{save | items: [instance | save.items]}
-        socket |> @socket.echo("You picked up the #{Format.item_name(item)}")
-        {:update, Map.put(state, :save, save)}
+        {:ok, item, Map.put(state, :save, save)}
 
       _ ->
-        socket |> @socket.echo(~s("#{item.name}" could not be found))
-        :ok
+        {:error, :could_not_pickup, item}
+    end
+  end
+
+  defp pick_up_currency(state) do
+    %{save: save} = state
+
+    case @room.pick_up_currency(save.room_id) do
+      {:ok, currency} ->
+        Logger.info(
+          "Session (#{inspect(self())}) picking up #{currency} currency from room (#{save.room_id})",
+          type: :player
+        )
+
+        save = %{save | currency: save.currency + currency}
+        {:ok, currency, Map.put(state, :save, save)}
+
+      _ ->
+        {:error, :could_not_pickup}
     end
   end
 end
--- test/game/command/pick_up_test.exs
+++ test/game/command/pick_up_test.exs
@@ -8,53 +8,77 @@ defmodule Game.Command.PickUpTest do
   alias Data.Item
   alias Data.Save
   alias Game.Command.PickUp
+  alias Game.Session.State
 
   setup do
     start_and_clear_items()
     item = create_item(%{name: "Short Sword", description: "A simple blade", keywords: ["sword"]})
     insert_item(item)
+
     @room.set_room(Map.merge(@room._room(), %{items: [Item.instantiate(item)]}))
-    @socket.clear_messages
-    {:ok, %{socket: :socket}}
+    @socket.clear_messages()
+
+    state = %State{
+      socket: :socket,
+      state: "active",
+      mode: "commands",
+      save: %Save{
+        room_id: 1,
+        items: [],
+        currency: 1,
+      }
+    }
+
+    {:ok, %{state: state}}
   end
 
-  test "pick up an item from a room", %{socket: socket} do
+  test "pick up an item from a room", %{state: state} do
     @room.clear_pick_up()
 
-    {:update, state} = PickUp.run({"sword"}, %{socket: socket, save: %Save{room_id: 1, items: []}})
+    {:update, state} = PickUp.run({"sword"}, state)
 
     assert state.save.items |> length == 1
 
-    [{^socket, look}] = @socket.get_echos()
+    [{_socket, look}] = @socket.get_echos()
     assert Regex.match?(~r(You picked up), look)
   end
 
-  test "item does not exist in the room", %{socket: socket} do
-    :ok = PickUp.run({"shield"}, %{socket: socket, save: %Save{room_id: 1, items: []}})
+  test "item does not exist in the room", %{state: state} do
+    :ok = PickUp.run({"shield"}, state)
 
-    [{^socket, look}] = @socket.get_echos()
+    [{_socket, look}] = @socket.get_echos()
     assert Regex.match?(~r("shield" could not be found), look)
   end
 
-  test "item has already been removed", %{socket: socket} do
+  test "item has already been removed", %{state: state} do
     @room.set_pick_up(:error)
 
     item = %Data.Item{id: 15, name: "shield"}
     room = %Data.Room{id: 1}
-    :ok = PickUp.pick_up(item, room, %{socket: socket, save: %Save{room_id: 1, items: []}})
-
-    [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r("shield" could not be found), look)
+    assert {:error, :could_not_pickup, ^item} = PickUp.pick_up(item, room, state)
   end
 
-  test "pick up gold from a room", %{socket: socket} do
+  test "pick up gold from a room", %{state: state} do
     @room.set_pick_up_currency({:ok, 100})
 
-    {:update, state} = PickUp.run({"gold"}, %{socket: socket, save: %Save{room_id: 1, currency: 1}})
+    {:update, state} = PickUp.run({"gold"}, state)
 
     assert state.save.currency == 101
 
-    [{^socket, look}] = @socket.get_echos()
+    [{_socket, look}] = @socket.get_echos()
     assert Regex.match?(~r(You picked up), look)
+  end
+
+  test "pick up all from a room", %{state: state} do
+    @room.set_pick_up_currency({:ok, 100})
+
+    {:update, state} = PickUp.run({:all}, state)
+
+    assert state.save.items |> length() == 1
+    assert state.save.currency == 101
+
+    [{_socket1, item}, {_socket2, currency}] = @socket.get_echos()
+    assert Regex.match?(~r(You picked up), item)
+    assert Regex.match?(~r(You picked up), currency)
   end
 end
