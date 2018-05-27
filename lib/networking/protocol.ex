7c99e2b04631527ef738266a4fde2dbad5c51380
Eric Oestrich <eric@oestrich.org>
2018-05-27 00:57:20 -0400

    Final touch ups for telnet connection

--- lib/networking/protocol.ex
+++ lib/networking/protocol.ex
@@ -13,7 +13,7 @@ defmodule Networking.Protocol do
   alias Web.Endpoint
   alias Web.Router.Helpers, as: RoutesHelper
 
-  @mudlet_version 9
+  @mudlet_version 10
 
   @behaviour :ranch_protocol
   @behaviour Networking.Socket
