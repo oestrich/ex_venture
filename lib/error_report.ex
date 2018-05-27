68630babaaf48d12004182dae81547d2056a01a8
Eric Oestrich <eric@oestrich.org>
2018-05-27 16:15:06 -0400

    Docs for ErrorReport

--- lib/error_report.ex
+++ lib/error_report.ex
@@ -1,6 +1,14 @@
 defmodule ErrorReport do
+  @moduledoc """
+  Error reporting to an external service
+  """
+
   @report_errors Application.get_env(:ex_venture, :errors)[:report]
 
+  @doc """
+  Report an error to the external service
+  """
+  @spec send_error(String.t()) :: :ok
   def send_error(message) do
     case @report_errors do
       true ->
