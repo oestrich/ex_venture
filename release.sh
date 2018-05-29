be86978cd848c22caa697d541d90395bdd6f4619
Eric Oestrich <eric@oestrich.org>
2018-05-29 17:51:08 -0400

    Force clean static assets on release
    The folder may not be there

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-rm -r priv/static/*
+rm -rf priv/static/*
 cd assets && node node_modules/brunch/bin/brunch build && cd ..
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release
