1f2f8f1947b0a61c64b201c190f7a95fe34c2f2c
Eric Oestrich <eric@oestrich.org>
2018-06-01 16:34:27 -0400

    Generate assets after packaging the release

--- release.sh
+++ release.sh
@@ -6,3 +6,4 @@ cd assets && node node_modules/brunch/bin/brunch build && cd ..
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release
 rm -r priv/static/*
+cd assets && node node_modules/brunch/bin/brunch build && cd ..
