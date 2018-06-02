7089b1133cc7362329b576f93e4df2878db7b821
Eric Oestrich <eric@oestrich.org>
2018-06-02 10:48:39 -0400

    Generate a production build of javascript

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 set -e
 
 rm -rf priv/static/*
-cd assets && node node_modules/brunch/bin/brunch build && cd ..
+cd assets && node node_modules/brunch/bin/brunch build -p && cd ..
 MIX_ENV=prod mix phx.digest
 MIX_ENV=prod mix release
 rm -r priv/static/*
