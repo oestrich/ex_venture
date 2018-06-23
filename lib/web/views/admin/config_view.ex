4ec96aa380b58fd7d2b626a221651b72f119351b
Eric Oestrich <eric@oestrich.org>
2018-06-23 17:25:31 -0400

    Update the starting save config from the web admin

--- lib/web/views/admin/config_view.ex
+++ lib/web/views/admin/config_view.ex
@@ -9,10 +9,18 @@ defmodule Web.Admin.ConfigView do
   def name("character_names"), do: "Random Character Names"
   def name("game_name"), do: "Game Name"
   def name("motd"), do: "Message of the Day"
+  def name("starting_save"), do: "Starting Save"
 
   def basic_stats_value(changeset) do
     changeset
-    |> Changeset.get_field("value", Config.basic_stats())
+    |> Changeset.get_field(:value, Config.basic_stats())
+    |> Poison.encode!(pretty: true)
+  end
+
+  def starting_save_value(changeset) do
+    changeset
+    |> Changeset.get_field(:value)
+    |> Poison.decode!()
     |> Poison.encode!(pretty: true)
   end
 end
