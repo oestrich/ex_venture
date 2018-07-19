8e03f19548b50eb438f67455553cda9eb9fd7fe0
Shane Sveller <shanesveller@gmail.com>
2018-07-19 18:21:39 -0500

    Add multi-stage Docker build

---
+++ Dockerfile
@@ -0,0 +1,53 @@
+FROM elixir:1.6.6-alpine as builder
+
+# The nuclear approach:
+# RUN apk add --no-cache alpine-sdk
+RUN apk add --no-cache \
+    gcc \
+    git \
+    make \
+    musl-dev
+
+RUN mix local.rebar --force && \
+    mix local.hex --force
+
+WORKDIR /app
+ENV MIX_ENV=prod
+COPY mix.* /app/
+RUN mix deps.get
+
+RUN mix deps.compile
+
+FROM node:8.6 as frontend
+
+WORKDIR /app
+COPY assets/package*.json /app/
+COPY --from=builder /app/deps/phoenix /deps/phoenix
+COPY --from=builder /app/deps/phoenix_html /deps/phoenix_html
+
+RUN npm install
+
+COPY assets /app
+RUN node node_modules/brunch/bin/brunch build --production
+
+FROM builder as releaser
+COPY --from=frontend /priv/static /app/priv/static
+COPY . /app/
+RUN mix phx.digest
+ARG APP_VERSION=0.23.0
+RUN cp config/prod.docker.exs config/prod.exs && \
+    mix release --env=prod --no-tar
+
+FROM alpine:3.7
+RUN apk add -U bash libssl1.0
+WORKDIR /app
+COPY --from=releaser /app/_build/prod/rel/ex_venture /app/
+
+ENV MIX_ENV=prod
+
+EXPOSE 4000 5555 5556
+
+VOLUME /var/log/ex_venture/
+
+ENTRYPOINT ["bin/ex_venture"]
+CMD ["foreground"]
---
+++ config/prod.docker.exs
@@ -0,0 +1,73 @@
+use Mix.Config
+
+version =
+  case System.cmd("git", ["rev-parse", "HEAD"]) do
+    {version, 0} -> version
+    _ -> System.get_env("APP_VERSION")
+  end
+
+config :ex_venture, version: String.trim(version)
+
+config :ex_venture, Data.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "ex_venture",
+  hostname: "postgres",
+  username: "ex_venture",
+  password: "ex_venture",
+  pool_size: 10
+
+config :ex_venture, Web.Endpoint,
+  http: [port: 4000],
+  url: [host: {:system, "HOST"}, port: 443, scheme: "https"],
+  server: true,
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :ex_venture, :networking,
+  host: {:system, "HOST"},
+  port: 5555,
+  ssl_port: {:system, "SSL_PORT"},
+  server: true,
+  socket_module: Networking.Protocol
+
+config :ex_venture, :game,
+  npc: Game.NPC,
+  zone: Game.Zone,
+  room: Game.Room,
+  environment: Game.Environment,
+  shop: Game.Shop,
+  zone: Game.Zone,
+  continue_wait: 500
+
+config :logger, level: :info
+
+config :logger,
+  backends: [
+    {LoggerFileBackend, :global},
+    {LoggerFileBackend, :phoenix},
+    {LoggerFileBackend, :commands}
+  ]
+
+config :logger, :global, path: "/var/log/ex_venture/global.log"
+
+config :logger, :phoenix,
+  path: "/var/log/ex_venture/phoenix.log",
+  level: :info,
+  metadata_filter: [type: :phoenix]
+
+config :logger, :commands,
+  path: "/var/log/ex_venture/commands.log",
+  level: :info,
+  metadata_filter: [type: :command]
+
+config :ex_venture, ExVenture.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: {:system, "SMTP_SERVER"},
+  port: {:system, "SMTP_PORT"},
+  username: {:system, "SMTP_USERNAME"},
+  password: {:system, "SMTP_PASSWORD"}
+
+config :ex_venture, :mailer, from: {:system, "EXVENTURE_MAILER_FROM"}
+
+if File.exists?("config/prod.secret.exs") do
+  import_config("prod.secret.exs")
+end
