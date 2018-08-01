0701d86a9960b2fcbf9048dcae0757d4ad4cc1ae
Eric Oestrich <eric@oestrich.org>
2018-07-31 21:48:08 -0400

    Bump the version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExVenture.Mixfile do
   def project do
     [
       app: :ex_venture,
-      version: "0.23.0",
+      version: "0.24.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
