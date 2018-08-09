ff2ef104372ef16bb20a6a06650986932f847df8
Eric Oestrich <eric@oestrich.org>
2018-08-08 21:32:54 -0400

    Add in quest step help into the admin

--- lib/web/views/admin/quest_step_view.ex
+++ lib/web/views/admin/quest_step_view.ex
@@ -12,4 +12,10 @@ defmodule Web.Admin.QuestStepView do
   def type_form("item/have"), do: "_item_have"
   def type_form("npc/kill"), do: "_npc_kill"
   def type_form("room/explore"), do: "_room_explore"
+
+  def type_help("item/collect"), do: "quest.item_collect"
+  def type_help("item/give"), do: "quest.item_give"
+  def type_help("item/have"), do: "quest.item_have"
+  def type_help("npc/kill"), do: "quest.npc_kill"
+  def type_help("room/explore"), do: "quest.room_explore"
 end
