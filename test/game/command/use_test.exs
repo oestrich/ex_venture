f4d9031524ba75084f11f32c1dfff2040580be3a
Eric Oestrich <eric@oestrich.org>
2018-09-17 17:42:15 -0400

    Rework use tests to be in a describe

--- test/game/command/use_test.exs
+++ test/game/command/use_test.exs
@@ -40,68 +40,70 @@ defmodule Game.Command.UseTest do
 
     user = %{base_user() | id: 1, save: save}
 
-    %{socket: :socket, user: user, save: user.save}
+    %{state: %{socket: :socket, user: user, save: user.save}}
   end
 
   describe "parsing a custom command" do
-    test "pick up an item's custom command", state do
+    test "pick up an item's custom command", %{state: state} do
       context = %ParseContext{player: state.user}
       assert {:use, "potion"} = Use.parse("drink potion", context)
     end
   end
 
-  test "use an item - removes if amount ends up as 0", state = %{socket: socket} do
-    Registry.register(state.user)
-    Registry.catch_up()
+  describe "using a normal item" do
+    test "use an item - removes if amount ends up as 0", %{state: state} do
+      Registry.register(state.user)
+      Registry.catch_up()
 
-    {:skip, :prompt, state} = Use.run({:use, "potion"}, state)
+      {:skip, :prompt, state} = Use.run({:use, "potion"}, state)
 
-    assert [] = state.save.items
+      assert [] = state.save.items
 
-    [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(Used a potion), look)
-    assert_receive {:"$gen_cast", {:apply_effects, [], {:player, %{id: 1}}, _}}
-  end
+      [{_socket, look}] = @socket.get_echos()
+      assert Regex.match?(~r(Used a potion), look)
+      assert_receive {:"$gen_cast", {:apply_effects, [], {:player, %{id: 1}}, _}}
+    end
 
-  test "use an item with an amount - decrements amount", state do
-    Registry.register(state.user)
-    Registry.catch_up()
+    test "use an item with an amount - decrements amount", %{state: state} do
+      Registry.register(state.user)
+      Registry.catch_up()
 
-    save =
-      base_save()
-      |> Map.put(:items, [%Item.Instance{id: 1, created_at: Timex.now(), amount: 2}])
+      save =
+        base_save()
+        |> Map.put(:items, [%Item.Instance{id: 1, created_at: Timex.now(), amount: 2}])
 
-    {:skip, :prompt, state} = Use.run({:use, "potion"}, %{state | save: save})
+      {:skip, :prompt, state} = Use.run({:use, "potion"}, %{state | save: save})
 
-    assert [%Item.Instance{amount: 1}] = state.save.items
-  end
+      assert [%Item.Instance{amount: 1}] = state.save.items
+    end
 
-  test "use an item with an amount - -1 is unlimited", state do
-    Registry.register(state.user)
-    Registry.catch_up()
+    test "use an item with an amount - -1 is unlimited", %{state: state} do
+      Registry.register(state.user)
+      Registry.catch_up()
 
-    save =
-      base_save()
-      |> Map.put(:items, [%Item.Instance{id: 1, created_at: Timex.now(), amount: -1}])
+      save =
+        base_save()
+        |> Map.put(:items, [%Item.Instance{id: 1, created_at: Timex.now(), amount: -1}])
 
-    assert {:skip, :prompt} = Use.run({:use, "potion"}, %{state | save: save})
-  end
+      assert {:skip, :prompt} = Use.run({:use, "potion"}, %{state | save: save})
+    end
 
-  test "trying to use a non-usable item", state = %{socket: socket} do
-    save =
-      base_save()
-      |> Map.put(:items, [item_instance(2)])
+    test "trying to use a non-usable item", %{state: state} do
+      save =
+        base_save()
+        |> Map.put(:items, [item_instance(2)])
 
-    :ok = Use.run({:use, "leather armor"}, %{state | save: save})
+      :ok = Use.run({:use, "leather armor"}, %{state | save: save})
 
-    [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(could not be used), look)
-  end
+      [{_socket, look}] = @socket.get_echos()
+      assert Regex.match?(~r(could not be used), look)
+    end
 
-  test "item not found", state = %{socket: socket} do
-    :ok = Use.run({:use, "poton"}, state)
+    test "item not found", %{state: state} do
+      :ok = Use.run({:use, "poton"}, state)
 
-    [{^socket, look}] = @socket.get_echos()
-    assert Regex.match?(~r(could not be found), look)
+      [{_socket, look}] = @socket.get_echos()
+      assert Regex.match?(~r(could not be found), look)
+    end
   end
 end
