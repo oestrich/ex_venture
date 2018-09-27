c5832eb7226414e34c1a95cf9d8eb703dc1c4b7f
Eric Oestrich <eric@oestrich.org>
2018-09-26 21:04:01 -0400

    Start on simple keyboard shortcuts

--- assets/play/js/keys.js
+++ assets/play/js/keys.js
@@ -1,6 +1,9 @@
+import _ from "underscore"
+
 export default class Keys {
   constructor() {
     this.keysDown = [];
+    this.listeners = {};
 
     document.addEventListener("keydown", e => {
       this.keyDown(e.key);
@@ -17,11 +20,23 @@ export default class Keys {
 
   keyDown(key) {
     this.keysDown.push(key);
+
+    if (this.listeners[this.keysDown] != undefined) {
+      _.each(this.listeners[this.keysDown], callback => {
+        callback();
+      });
+    }
   }
 
   keyUp(keyDown) {
     this.keysDown = this.keysDown.filter(key => {
       return key != keyDown;
     });
+  }
+
+  on(event, callback) {
+    let listeners = this.listeners[event] || [];
+    listeners.push(callback);
+    this.listeners[event] = listeners;
   }
 }
