e7be6500eb017e32699fe9f7c039b2d0e6a39b53
lorecrafting <raymond.n.luong@gmail.com>
2018-11-18 08:11:51 -1000

    [react-ui] setup initial reducers

--- assets/play_react/js/redux/reducers/reducers.js
+++ assets/play_react/js/redux/reducers/reducers.js
@@ -19,14 +19,19 @@ const initialState = {
 export default function(state = initialState, action) {
   switch (action.type) {
     case UPDATE_ROOM_INFO: {
+      return { ...state, roomInfo: action.payload };
     }
     case UPDATE_ZONE_MAP: {
+      return { ...state, zoneMap: action.payload };
     }
     case UPDATE_CHARACTER_INFO: {
+      return { ...state, characterInfo: action.payload };
     }
     case UPDATE_CHARACTER_VITALS: {
+      return { ...state, characterVitals: action.payload };
     }
     case UPDATE_CHARACTER_SKILLS: {
+      return { ...state, characterSkills: action.payload };
     }
     case UPDATE_EVENT_STREAM: {
       return { ...state, eventStream: [...state.eventStream, action.payload] };
