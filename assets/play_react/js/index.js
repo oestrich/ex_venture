c4142c8abd403733657e879ead0c769b299667f5
lorecrafting <raymond.n.luong@gmail.com>
2018-11-17 17:00:47 -1000

    [react-ui] create phoenix socket connection

--- assets/play_react/js/index.js
+++ assets/play_react/js/index.js
@@ -5,6 +5,27 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import ReduxStore from './redux/store.js';
 
+// FIX: For testing phoenix conx
+import { channel } from './socket.js';
+channel.on('gmcp', data => {
+  console.log('[Channel: GMCP]', data);
+});
+
+channel.on('option', data => {
+  console.log('[Channel: OPTION', data);
+});
+channel.on('prompt', data => {
+  console.log('[Channel: PROMPT', data);
+});
+channel.on('echo', data => {
+  console.log('[Channel: ECHO', data);
+});
+channel.on('disconnect', data => {
+  console.log('[Channel: DISCONNECT', data);
+});
+console.log('channel', channel);
+window.channel = channel;
+
 const reactRootElement = document.getElementById('react-root');
 ReactDOM.render(
   <Provider store={ReduxStore}>
--- assets/play_react/js/socket.js
+++ assets/play_react/js/socket.js
@@ -1,8 +1,15 @@
 // Initialize phoenix websocket connection
 import { Socket } from 'phoenix';
+import { guid } from './utils.js';
 
 const characterToken = body.getAttribute('data-character-token');
-const socket = new Socket('/socket', { params: { token: characterToken } });
+const socket = new Socket('/socket', {
+  params: {
+    token: characterToken
+  }
+});
 socket.connect();
+let channel = socket.channel('telnet:' + 'webclient:' + guid(), {});
+channel.join();
 
-export default socket;
+export { channel, socket };
