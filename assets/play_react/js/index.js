1f3b4d7eaee31f3bb87ebe218c8b08a7ee5645c6
lorecrafting <raymond.n.luong@gmail.com>
2018-11-17 17:00:47 -1000

    [react-ui] create phoenix socket connection

--- assets/play_react/js/index.js
+++ assets/play_react/js/index.js
@@ -5,6 +5,26 @@ import App from './App.jsx';
 import { Provider } from 'react-redux';
 import ReduxStore from './redux/store.js';
 
+// FIX: For testing phoenix conx
+import { channel } from './socket.js';
+channel.on('gmcp', data => {
+  console.log('[Channel: GMCP]', data);
+});
+
+channel.on('option', data => {
+  console.log('[Channel: OPTION]', data);
+});
+channel.on('prompt', data => {
+  console.log('[Channel: PROMPT]', data);
+});
+channel.on('echo', data => {
+  console.log('[Channel: ECHO]', data);
+});
+channel.on('disconnect', data => {
+  console.log('[Channel: DISCONNECT]', data);
+});
+window.channel = channel;
+
 const reactRootElement = document.getElementById('react-root');
 ReactDOM.render(
   <Provider store={ReduxStore}>
