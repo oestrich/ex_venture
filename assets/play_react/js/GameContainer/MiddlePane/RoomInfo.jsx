1cd3561f697db31578dab8c37b2bd0bd755c24f5
lorecrafting <raymond.n.luong@gmail.com>
2018-11-18 15:12:00 -1000

    [react-ui] parse roomInfo data

--- assets/play_react/js/GameContainer/MiddlePane/RoomInfo.jsx
+++ assets/play_react/js/GameContainer/MiddlePane/RoomInfo.jsx
@@ -2,9 +2,36 @@ import React from 'react';
 import styled, { css } from 'styled-components';
 import { connect } from 'react-redux';
 
-const RoomInfo = ({ className, roomInfo }) => (
-  <div className={className}> RoomInfo {roomInfo} </div>
-);
+const RoomInfo = ({ className, roomInfo }) => {
+  console.log('roomInfo', roomInfo);
+  const { name, description, players, npcs, shops, items, exits } = roomInfo;
+  return (
+    <div className={className}>
+      <div>{name}</div>
+      <br />
+      <div>{description}</div>
+      <br />
+      {players ? players.map(player => <span>{player.name} </span>) : null}
+      {npcs ? npcs.map(npc => <span>{npc.name} </span>) : null}
+      {shops ? shops.map(shop => <span>{shop.name} </span>) : null}
+      {items ? items.map(item => <span>{item.name} </span>) : null}
+      <br />
+      {exits ? exits.map(exit => <span>{exit.direction} </span>) : null}
+    </div>
+  );
+};
+
+RoomInfo.defaultProps = {
+  roomInfo: {
+    name: '',
+    description: '',
+    players: [],
+    npcs: [],
+    shops: [],
+    items: [],
+    exits: []
+  }
+};
 
 const mapStateToProps = state => {
   return { roomInfo: state.roomInfo };
