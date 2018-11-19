e7dc047b666d1053a7a42a75bcf5b2c8eb7e2098
lorecrafting <raymond.n.luong@gmail.com>
2018-11-18 16:08:22 -1000

    [react-ui] stay scrolled to bottom in room event component

--- assets/play_react/js/GameContainer/MiddlePane/RoomEvents.jsx
+++ assets/play_react/js/GameContainer/MiddlePane/RoomEvents.jsx
@@ -1,24 +1,69 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import styled, { css } from 'styled-components';
 
-const RoomEvents = ({ className, eventStream }) => {
-  return (
-    <div className={className}>
-      RoomEvents
-      <div>
-        {eventStream.map(event => {
-          return (
-            <div>
-              <div>{event}</div>
-              <br />
-            </div>
-          );
-        })}
+// const RoomEvents = ({ className, eventStream }) => {
+//   return (
+//     <div className={className}>
+//       RoomEvents
+//       <div>
+//         {eventStream.map(event => {
+//           return (
+//             <div>
+//               <div>{event}</div>
+//               <br />
+//             </div>
+//           );
+//         })}
+//         <div
+//           ref={el => {
+//             this.messagseEnd = el;
+//           }}
+//         />
+//       </div>
+//     </div>
+//   );
+// };
+
+class RoomEvents extends Component {
+  constructor(props) {
+    super(props);
+    this.scrollToBottom = this.scrollToBottom.bind(this);
+  }
+  scrollToBottom() {
+    this.messagesEnd.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  componentDidMount() {
+    this.scrollToBottom();
+  }
+
+  componentDidUpdate() {
+    this.scrollToBottom();
+  }
+  render() {
+    return (
+      <div className={this.props.className}>
+        RoomEvents
+        <div>
+          {this.props.eventStream.map(event => {
+            return (
+              <div>
+                <div>{event}</div>
+                <br />
+              </div>
+            );
+          })}
+          <div
+            ref={el => {
+              this.messagesEnd = el;
+            }}
+          />
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 RoomEvents.defaultProps = {
   eventStream: []
