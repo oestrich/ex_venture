f8c5aaa1c4037b5eef23b7b062e4d898a4a50212
lorecrafting <raymond.n.luong@gmail.com>
2018-11-25 08:56:40 -1000

    [react-ui] add styled status bars

--- assets/play_react/js/GameContainer/MiddlePane/PlayerHud/StatusBar.jsx
+++ assets/play_react/js/GameContainer/MiddlePane/PlayerHud/StatusBar.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const StatusBar = () => <div> StatusBar </div>;
+const FlexColumn = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding-top: 5px;
+`;
+
+const Bar = styled.div`
+  flex: 1;
+  display: inline-block;
+  color: #444;
+  border: 1px solid #6177c8;
+  background: #879ade;
+  box-shadow: 0 2px 5px 0px #000000;
+  border-radius: 5px;
+  vertical-align: middle;
+  max-height: 35px;
+  height: 5px;
+  padding: 5px;
+  text-align: center;
+`;
+
+const StatusBar = props => {
+  return (
+    <FlexColumn>
+      <Bar />
+      <Bar />
+      <Bar />
+    </FlexColumn>
+  );
+};
 
 export default StatusBar;
