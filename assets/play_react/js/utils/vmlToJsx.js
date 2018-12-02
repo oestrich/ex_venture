9b53ca3c95c8b682233b680d2f33afe82973be9a
lorecrafting <raymond.n.luong@gmail.com>
2018-12-01 16:54:28 -1000

    :art: [react-ui] restructure vmlToAst util function

--- assets/play_react/js/utils/vmlToJsx.js
+++ assets/play_react/js/utils/vmlToJsx.js
@@ -1,61 +1,5 @@
-import React from 'react';
-
 // This was originally an html parser written by @HenrikJoreteg and @Philip_Roberts.
 // The original repo this was taken from: https://github.com/HenrikJoreteg/html-parse-stringify
-
-var attrRE = /([\w-]+)|['"]{1}([^'"]*)['"]{1}/g;
-
-// create optimized lookup object for
-// void elements as listed here:
-// http://www.w3.org/html/wg/drafts/html/master/syntax.html#void-elements
-var lookup = Object.create ? Object.create(null) : {};
-lookup.area = true;
-lookup.base = true;
-lookup.br = true;
-lookup.col = true;
-lookup.embed = true;
-lookup.hr = true;
-lookup.img = true;
-lookup.input = true;
-lookup.keygen = true;
-lookup.link = true;
-lookup.menuitem = true;
-lookup.meta = true;
-lookup.param = true;
-lookup.source = true;
-lookup.track = true;
-lookup.wbr = true;
-
-function parseTag(tag) {
-  var i = 0;
-  var key;
-  var res = {
-    type: 'tag',
-    name: '',
-    voidElement: false,
-    attrs: {},
-    children: []
-  };
-
-  tag.replace(attrRE, function(match) {
-    if (i % 2) {
-      key = match;
-    } else {
-      if (i === 0) {
-        if (lookup[match] || tag.charAt(tag.length - 2) === '/') {
-          res.voidElement = true;
-        }
-        res.name = match;
-      } else {
-        res.attrs[key] = match.replace(/['"]/g, '');
-      }
-    }
-    i++;
-  });
-
-  return res;
-}
-
 /*jshint -W030 */
 // re-used obj for quick lookups of components
 var empty = Object.create ? Object.create(null) : {};
@@ -145,4 +89,58 @@ export function vmlToAst(html, options) {
   });
 
   return result;
+}
+
+// ***BEGIN TAG PARSING FUNCTIONALITY***
+
+var attrRE = /([\w-]+)|['"]{1}([^'"]*)['"]{1}/g;
+// create optimized lookup object for
+// void elements as listed here:
+// http://www.w3.org/html/wg/drafts/html/master/syntax.html#void-elements
+var lookup = Object.create ? Object.create(null) : {};
+lookup.area = true;
+lookup.base = true;
+lookup.br = true;
+lookup.col = true;
+lookup.embed = true;
+lookup.hr = true;
+lookup.img = true;
+lookup.input = true;
+lookup.keygen = true;
+lookup.link = true;
+lookup.menuitem = true;
+lookup.meta = true;
+lookup.param = true;
+lookup.source = true;
+lookup.track = true;
+lookup.wbr = true;
+
+function parseTag(tag) {
+  var i = 0;
+  var key;
+  var res = {
+    type: 'tag',
+    name: '',
+    voidElement: false,
+    attrs: {},
+    children: []
+  };
+
+  tag.replace(attrRE, function(match) {
+    if (i % 2) {
+      key = match;
+    } else {
+      if (i === 0) {
+        if (lookup[match] || tag.charAt(tag.length - 2) === '/') {
+          res.voidElement = true;
+        }
+        res.name = match;
+      } else {
+        res.attrs[key] = match.replace(/['"]/g, '');
+      }
+    }
+    i++;
+  });
+
+  return res;
 }
