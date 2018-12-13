f6548edc607f5efa2d22421b574f5913145a7da6
Eric Oestrich <eric@oestrich.org>
2018-12-12 22:07:11 -0500

    Setup firewall, install nginx/certbot, template files

---
+++ deploy/files/exventure.config.exs
@@ -0,0 +1,30 @@
+use Mix.Config
+
+config :ex_venture, Data.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  database: "exventure",
+  port: "5432",
+  pool_size: 20
+
+config :ex_venture, ExVenture.Mailer,
+  adapter: Bamboo.SMTPAdapter,
+  server: "",
+  port: "587",
+  username: "",
+  password: ""
+
+config :ex_venture, :mailer, from: ""
+
+config :ex_venture, Web.Endpoint,
+  url: [host: "", port: 443, scheme: "https"]
+
+config :ex_venture, :networking,
+  host: "",
+  ssl_port: "5443"
+
+config :pid_file, file: "/home/deploy/ex_venture.pid"
+
+config :ex_venture, :errors, report: false
+
+config :gossip, :client_id, ""
+config :gossip, :client_secret, ""
