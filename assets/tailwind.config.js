ff23386742ebef7e284fbacf4d8398309b7a808b
Eric Oestrich <eric@oestrich.org>
2020-10-31 21:57:25 -0400

    Set up a slightly larger than laptop breakpoint size

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
   purge: [],
   theme: {
     extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      'xxl': '1500px',
+    },
   },
   variants: {},
   plugins: [],
