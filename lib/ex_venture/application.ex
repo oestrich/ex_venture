c4c55270551fb48d8ff6f275b646c7853c5af4cd
Eric Oestrich <eric@oestrich.org>
2020-12-04 19:24:35 -0500

    Move Kantele.Application into ExVenture.Application
    Also host the websocket listener in the phoenix endpoint

--- lib/ex_venture/application.ex
+++ lib/ex_venture/application.ex
@@ -10,7 +10,7 @@ defmodule ExVenture.Application do
       {Phoenix.PubSub, name: ExVenture.PubSub},
       ExVenture.Config.Cache,
       ExVenture.Repo,
-      Kantele.Application,
+      ExVenture.Application.KalevalaSupervisor,
       Web.Endpoint
     ]
 
@@ -23,5 +23,74 @@ defmodule ExVenture.Application do
   def config_change(changed, _new, removed) do
     Web.Endpoint.config_change(changed, removed)
     :ok
+  end
+end
+
+defmodule ExVenture.Application.KalevalaSupervisor do
+  @moduledoc false
+
+  use Supervisor
+
+  def foreman_options() do
+    [
+      supervisor_name: Kantele.Character.Foreman.Supervisor,
+      communication_module: Kantele.Communication,
+      initial_controller: Kantele.Character.LoginController,
+      presence_module: Kantele.Character.Presence,
+      quit_view: {Kantele.Character.QuitView, "disconnected"}
+    ]
+  end
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, [], opts)
+  end
+
+  def init(_args) do
+    telnet_config = [
+      telnet: [
+        port: 4646
+      ],
+      protocol: [
+        output_processors: [
+          Kalevala.Output.Tags,
+          Kantele.Output.AdminTags,
+          Kantele.Output.SemanticColors,
+          Kalevala.Output.Tables,
+          Kalevala.Output.TagColors,
+          Kalevala.Output.StripTags
+        ]
+      ],
+      foreman: foreman_options()
+    ]
+
+    children = [
+      {Kantele.Config, [name: Kantele.Config]},
+      {Kantele.Communication, []},
+      {Kalevala.Help, [name: Kantele.Help]},
+      {Kantele.World, []},
+      {Kantele.Character.Presence, []},
+      {Kantele.Character.Emotes, [name: Kantele.Character.Emotes]},
+      {Kalevala.Character.Foreman.Supervisor, [name: Kantele.Character.Foreman.Supervisor]},
+      telnet_listener(telnet_config)
+    ]
+
+    children =
+      Enum.reject(children, fn child ->
+        is_nil(child)
+      end)
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+
+  def telnet_listener(telnet_config) do
+    config = Application.get_env(:ex_venture, :listener, [])
+
+    case Keyword.get(config, :start, true) do
+      true ->
+        {Kalevala.Telnet.Listener, telnet_config}
+
+      false ->
+        nil
+    end
   end
 end
--- lib/web/endpoint.ex
+++ lib/web/endpoint.ex
@@ -3,6 +3,7 @@ defmodule Web.Endpoint do
 
   use Phoenix.Endpoint, otp_app: :ex_venture
 
+  alias ExVenture.Application.KalevalaSupervisor
   alias ExVenture.Config
 
   socket "/socket", Web.UserSocket,
@@ -57,9 +58,33 @@ defmodule Web.Endpoint do
   def init(_type, config) do
     vapor_config = Config.endpoint()
 
+    websocket_config = %{
+      handler: [
+        output_processors: [
+          Kalevala.Output.Tags,
+          Kantele.Output.AdminTags,
+          Kantele.Output.SemanticColors,
+          Kantele.Output.Tooltips,
+          Kantele.Output.Commands,
+          Kalevala.Output.Tables,
+          Kalevala.Output.Websocket
+        ]
+      ],
+      foreman: KalevalaSupervisor.foreman_options()
+    }
+
     config =
       Keyword.merge(config,
-        http: [port: vapor_config.http_port],
+        http: [
+          port: vapor_config.http_port,
+          dispatch: [
+            {:_,
+             [
+               {"/socket", Kalevala.Websocket.Handler, websocket_config},
+               {:_, Phoenix.Endpoint.Cowboy2Handler, {Web.Endpoint, []}}
+             ]}
+          ]
+        ],
         secret_key_base: vapor_config.secret_key_base,
         url: [
           host: vapor_config.url_host,
