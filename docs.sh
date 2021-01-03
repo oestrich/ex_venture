d7dcd83685b400e8be05f4719c3ff563bb3108fd
Eric Oestrich <eric@oestrich.org>
2021-01-02 19:55:57 -0500

    Update docs.sh to be able to deploy

--- docs.sh
+++ docs.sh
@@ -9,4 +9,4 @@ docker build -f Dockerfile.site -t oestrich/exventure.org:${sha} .
 docker push oestrich/exventure.org:${sha}
 
 cd helm
-helm upgrade exventure static/ --namespace static-sites -f values.yaml --set image.tag=${sha}
+helm upgrade exventure static/ --namespace static-sites -f values.yml --set image.tag=${sha}
